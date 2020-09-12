42e0af289ca85d75745967a796f88f308679bc4a
doadin <tbkizle@gmail.com>
2020-09-12 00:56:28 -0400

    Update safelist.sh

--- safelist.sh
+++ safelist.sh
@@ -1,215 +1,218 @@
 #!/bin/bash
 #Google Maps and other Google services
-pihole -w clients4.google.com
-pihole -w clients2.google.com
+pihole -w -q -nr clients4.google.com
+pihole -w -q -nr clients2.google.com
 #YouTube history
-pihole -w s.youtube.com
-pihole -w video-stats.l.google.com
+pihole -w -q -nr s.youtube.com
+pihole -w -q -nr video-stats.l.google.com
 #Google Play
-pihole -w android.clients.google.com
+pihole -w -q -nr android.clients.google.com
 #Google Keep
-pihole -w reminders-pa.googleapis.com
-pihole -w firestore.googleapis.com
+pihole -w -q -nr reminders-pa.googleapis.com
+pihole -w -q -nr firestore.googleapis.com
 #Gmail (Google Mail)
-pihole -w googleapis.l.google.com
+pihole -w -q -nr googleapis.l.google.com
 #Google Chrome (to update on ubuntu)
-pihole -w dl.google.com
+pihole -w -q -nr dl.google.com
 #Windows uses this to verify connectivity to Internet
-pihole -w www.msftncsi.com
+pihole -w -q -nr www.msftncsi.com
 #Microsoft Web Pages (Outlook, Office365, Live, Microsoft.com 569...)
-pihole -w outlook.office365.com 
-pihole -w products.office.com 
-pihole -w c.s-microsoft.com 
-pihole -w i.s-microsoft.com 
-pihole -w login.live.com 
-pihole -w login.microsoftonline.com
+pihole -w -q -nr outlook.office365.com 
+pihole -w -q -nr products.office.com 
+pihole -w -q -nr c.s-microsoft.com 
+pihole -w -q -nr i.s-microsoft.com 
+pihole -w -q -nr login.live.com 
+pihole -w -q -nr login.microsoftonline.com
 #Backup bitlocker recovery key to Microsoft account
-pihole -w g.live.com
+pihole -w -q -nr g.live.com
 #Microsoft Store (Windows Store)
-pihole -w dl.delivery.mp.microsoft.com 
-pihole -w geo-prod.do.dsp.mp.microsoft.com 
-pihole -w displaycatalog.mp.microsoft.com
+pihole -w -q -nr dl.delivery.mp.microsoft.com 
+pihole -w -q -nr geo-prod.do.dsp.mp.microsoft.com 
+pihole -w -q -nr displaycatalog.mp.microsoft.com
 #Windows 10 Update
-pihole -w sls.update.microsoft.com.akadns.net 
-pihole -w fe3.delivery.dsp.mp.microsoft.com.nsatc.net
+pihole -w -q -nr sls.update.microsoft.com.akadns.net 
+pihole -w -q -nr fe3.delivery.dsp.mp.microsoft.com.nsatc.net
 #Xbox Live
-pihole -w clientconfig.passport.net 
-pihole -w v10.events.data.microsoft.com
-pihole -w v20.events.data.microsoft.com
-pihole -w client-s.gateway.messenger.live.com
-pihole -w xbox.ipv6.microsoft.com 
-pihole -w device.auth.xboxlive.com 
-pihole -w www.msftncsi.com 
-pihole -w title.mgt.xboxlive.com 
-pihole -w xsts.auth.xboxlive.com 
-pihole -w title.auth.xboxlive.com 
-pihole -w ctldl.windowsupdate.com 
-pihole -w attestation.xboxlive.com 
-pihole -w xboxexperiencesprod.experimentation.xboxlive.com 
-pihole -w xflight.xboxlive.com 
-pihole -w cert.mgt.xboxlive.com 
-pihole -w xkms.xboxlive.com 
-pihole -w def-vef.xboxlive.com 
-pihole -w notify.xboxlive.com 
-pihole -w help.ui.xboxlive.com 
-pihole -w licensing.xboxlive.com 
-pihole -w eds.xboxlive.com 
-pihole -w www.xboxlive.com 
-pihole -w v10.vortex-win.data.microsoft.com 
-pihole -w settings-win.data.microsoft.com
+pihole -w -q -nr clientconfig.passport.net 
+pihole -w -q -nr v10.events.data.microsoft.com
+pihole -w -q -nr v20.events.data.microsoft.com
+pihole -w -q -nr client-s.gateway.messenger.live.com
+pihole -w -q -nr xbox.ipv6.microsoft.com 
+pihole -w -q -nr device.auth.xboxlive.com 
+pihole -w -q -nr www.msftncsi.com 
+pihole -w -q -nr title.mgt.xboxlive.com 
+pihole -w -q -nr xsts.auth.xboxlive.com 
+pihole -w -q -nr title.auth.xboxlive.com 
+pihole -w -q -nr ctldl.windowsupdate.com 
+pihole -w -q -nr attestation.xboxlive.com 
+pihole -w -q -nr xboxexperiencesprod.experimentation.xboxlive.com 
+pihole -w -q -nr xflight.xboxlive.com 
+pihole -w -q -nr cert.mgt.xboxlive.com 
+pihole -w -q -nr xkms.xboxlive.com 
+pihole -w -q -nr def-vef.xboxlive.com 
+pihole -w -q -nr notify.xboxlive.com 
+pihole -w -q -nr help.ui.xboxlive.com 
+pihole -w -q -nr licensing.xboxlive.com 
+pihole -w -q -nr eds.xboxlive.com 
+pihole -w -q -nr www.xboxlive.com 
+pihole -w -q -nr v10.vortex-w -q -nrin.data.microsoft.com 
+pihole -w -q -nr settings-w -q -nrin.data.microsoft.com
 #Skype
-pihole -w s.gateway.messenger.live.com 
-pihole -w client-s.gateway.messenger.live.com 
-pihole -w ui.skype.com pricelist.skype.com 
-pihole -w apps.skype.com m.hotmail.com 
-pihole -w sa.symcb.com 
-pihole -w s{1..5}.symcb.com
+pihole -w -q -nr s.gateway.messenger.live.com 
+pihole -w -q -nr client-s.gateway.messenger.live.com 
+pihole -w -q -nr ui.skype.com pricelist.skype.com 
+pihole -w -q -nr apps.skype.com m.hotmail.com 
+pihole -w -q -nr sa.symcb.com 
+pihole -w -q -nr s{1..5}.symcb.com
 #Microsoft Office
-pihole -w officeclient.microsoft.com
+pihole -w -q -nr officeclient.microsoft.com
 #Spotify
-pihole -w spclient.wg.spotify.com 
-pihole -w apresolve.spotify.com
+pihole -w -q -nr spclient.wg.spotify.com 
+pihole -w -q -nr apresolve.spotify.com
 #Target's Weekly Ads
-pihole -w weeklyad.target.com 
-pihole -w m.weeklyad.target.com 
-pihole -w weeklyad.target.com.edgesuite.net
+pihole -w -q -nr weeklyad.target.com 
+pihole -w -q -nr m.weeklyad.target.com 
+pihole -w -q -nr weeklyad.target.com.edgesuite.net
 #Facebook, Facebook Messenger
-pihole -w upload.facebook.com 
-pihole -w creative.ak.fbcdn.net 
-pihole -w external-lhr0-1.xx.fbcdn.net 
-pihole -w external-lhr1-1.xx.fbcdn.net 
-pihole -w external-lhr10-1.xx.fbcdn.net 
-pihole -w external-lhr2-1.xx.fbcdn.net 
-pihole -w external-lhr3-1.xx.fbcdn.net 
-pihole -w external-lhr4-1.xx.fbcdn.net 
-pihole -w external-lhr5-1.xx.fbcdn.net 
-pihole -w external-lhr6-1.xx.fbcdn.net 
-pihole -w external-lhr7-1.xx.fbcdn.net 
-pihole -w external-lhr8-1.xx.fbcdn.net 
-pihole -w external-lhr9-1.xx.fbcdn.net 
-pihole -w fbcdn-creative-a.akamaihd.net 
-pihole -w scontent-lhr3-1.xx.fbcdn.net 
-pihole -w scontent.xx.fbcdn.net 
-pihole -w scontent.fgdl5-1.fna.fbcdn.net 
-pihole -w graph.facebook.com 
-pihole -w b-graph.facebook.com 
-pihole -w connect.facebook.com 
-pihole -w cdn.fbsbx.com 
-pihole -w api.facebook.com 
-pihole -w edge-mqtt.facebook.com 
-pihole -w mqtt.c10r.facebook.com 
-pihole -w portal.fb.com 
-pihole -w star.c10r.facebook.com 
-pihole -w star-mini.c10r.facebook.com 
-pihole -w b-api.facebook.com 
-pihole -w fb.me 
-pihole -w bigzipfiles.facebook.com 
-pihole -w l.facebook.com
+pihole -w -q -nr upload.facebook.com 
+pihole -w -q -nr creative.ak.fbcdn.net 
+pihole -w -q -nr external-lhr0-1.xx.fbcdn.net 
+pihole -w -q -nr external-lhr1-1.xx.fbcdn.net 
+pihole -w -q -nr external-lhr10-1.xx.fbcdn.net 
+pihole -w -q -nr external-lhr2-1.xx.fbcdn.net 
+pihole -w -q -nr external-lhr3-1.xx.fbcdn.net 
+pihole -w -q -nr external-lhr4-1.xx.fbcdn.net 
+pihole -w -q -nr external-lhr5-1.xx.fbcdn.net 
+pihole -w -q -nr external-lhr6-1.xx.fbcdn.net 
+pihole -w -q -nr external-lhr7-1.xx.fbcdn.net 
+pihole -w -q -nr external-lhr8-1.xx.fbcdn.net 
+pihole -w -q -nr external-lhr9-1.xx.fbcdn.net 
+pihole -w -q -nr fbcdn-creative-a.akamaihd.net 
+pihole -w -q -nr scontent-lhr3-1.xx.fbcdn.net 
+pihole -w -q -nr scontent.xx.fbcdn.net 
+pihole -w -q -nr scontent.fgdl5-1.fna.fbcdn.net 
+pihole -w -q -nr graph.facebook.com 
+pihole -w -q -nr b-graph.facebook.com 
+pihole -w -q -nr connect.facebook.com 
+pihole -w -q -nr cdn.fbsbx.com 
+pihole -w -q -nr api.facebook.com 
+pihole -w -q -nr edge-mqtt.facebook.com 
+pihole -w -q -nr mqtt.c10r.facebook.com 
+pihole -w -q -nr portal.fb.com 
+pihole -w -q -nr star.c10r.facebook.com 
+pihole -w -q -nr star-mini.c10r.facebook.com 
+pihole -w -q -nr b-api.facebook.com 
+pihole -w -q -nr fb.me 
+pihole -w -q -nr bigzipfiles.facebook.com 
+pihole -w -q -nr l.facebook.com
 #DirectTV
-pihole -w directvnow.com 
-pihole -w directvapplications.hb.omtrdc.net 
-pihole -w s.zkcdn.net 
-pihole -w js.maxmind.com
+pihole -w -q -nr directvnow.com 
+pihole -w -q -nr directvapplications.hb.omtrdc.net 
+pihole -w -q -nr s.zkcdn.net 
+pihole -w -q -nr js.maxmind.com
 #Plex
-pihole -w plex.tv 
-pihole -w tvdb2.plex.tv 
-pihole -w pubsub.plex.bz 
-pihole -w proxy.plex.bz 
-pihole -w proxy02.pop.ord.plex.bz 
-pihole -w cpms.spop10.ams.plex.bz 
-pihole -w meta-db-worker02.pop.ric.plex.bz meta.plex.bz 
-pihole -w tvthemes.plexapp.com.cdn.cloudflare.net 
-pihole -w tvthemes.plexapp.com 
-pihole -w 106c06cd218b007d-b1e8a1331f68446599e96a4b46a050f5.ams.plex.services 
-pihole -w meta.plex.tv 
-pihole -w cpms35.spop10.ams.plex.bz 
-pihole -w proxy.plex.tv 
-pihole -w metrics.plex.tv 
-pihole -w pubsub.plex.tv 
-pihole -w status.plex.tv 
-pihole -w www.plex.tv 
-pihole -w node.plexapp.com 
-pihole -w nine.plugins.plexapp.com 
-pihole -w staging.plex.tv 
-pihole -w app.plex.tv 
-pihole -w o1.email.plex.tv  
-pihole -w o2.sg0.plex.tv 
-pihole -w dashboard.plex.tv
-pihole -w gravatar.com
-pihole -w thetvdb.com
-pihole -w themoviedb.com
+pihole -w -q -nr plex.tv 
+pihole -w -q -nr tvdb2.plex.tv 
+pihole -w -q -nr pubsub.plex.bz 
+pihole -w -q -nr proxy.plex.bz 
+pihole -w -q -nr proxy02.pop.ord.plex.bz 
+pihole -w -q -nr cpms.spop10.ams.plex.bz 
+pihole -w -q -nr meta-db-w -q -nrorker02.pop.ric.plex.bz meta.plex.bz 
+pihole -w -q -nr tvthemes.plexapp.com.cdn.cloudflare.net 
+pihole -w -q -nr tvthemes.plexapp.com 
+pihole -w -q -nr 106c06cd218b007d-b1e8a1331f68446599e96a4b46a050f5.ams.plex.services 
+pihole -w -q -nr meta.plex.tv 
+pihole -w -q -nr cpms35.spop10.ams.plex.bz 
+pihole -w -q -nr proxy.plex.tv 
+pihole -w -q -nr metrics.plex.tv 
+pihole -w -q -nr pubsub.plex.tv 
+pihole -w -q -nr status.plex.tv 
+pihole -w -q -nr www.plex.tv 
+pihole -w -q -nr node.plexapp.com 
+pihole -w -q -nr nine.plugins.plexapp.com 
+pihole -w -q -nr staging.plex.tv 
+pihole -w -q -nr app.plex.tv 
+pihole -w -q -nr o1.email.plex.tv  
+pihole -w -q -nr o2.sg0.plex.tv 
+pihole -w -q -nr dashboard.plex.tv
+pihole -w -q -nr gravatar.com
+pihole -w -q -nr thetvdb.com
+pihole -w -q -nr themoviedb.com
 #Sonarr
-pihole -w services.sonarr.tv 
-pihole -w skyhook.sonarr.tv 
-pihole -w download.sonarr.tv 
-pihole -w apt.sonarr.tv 
-pihole -w forums.sonarr.tv
+pihole -w -q -nr services.sonarr.tv 
+pihole -w -q -nr skyhook.sonarr.tv 
+pihole -w -q -nr download.sonarr.tv 
+pihole -w -q -nr apt.sonarr.tv 
+pihole -w -q -nr forums.sonarr.tv
 #Placehold.it (Image placeholders often used during web design. Not sure why this is even blocked in the first place.)
-pihole -w placehold.it 
-pihole -w placeholdit.imgix.net
+pihole -w -q -nr placehold.it 
+pihole -w -q -nr placeholdit.imgix.net
 #Dropbox
-pihole -w dl.dropboxusercontent.com 
-pihole -w ns1.dropbox.com 
-pihole -w ns2.dropbox.com
+pihole -w -q -nr dl.dropboxusercontent.com 
+pihole -w -q -nr ns1.dropbox.com 
+pihole -w -q -nr ns2.dropbox.com
 #Fox News
-pihole -w widget-cdn.rpxnow.com
+pihole -w -q -nr widget-cdn.rpxnow.com
 #Images on Marketwatch.com
-pihole -w s.marketwatch.com
+pihole -w -q -nr s.marketwatch.com
 #Apple Music
-pihole -w itunes.apple.com
-pihole -w s.mzstatic.com
+pihole -w -q -nr itunes.apple.com
+pihole -w -q -nr s.mzstatic.com
 #GoDaddy webmail buttons
-pihole -w imagesak.secureserver.net
+pihole -w -q -nr imagesak.secureserver.net
 #Apple ID
-pihole -w appleid.apple.com
+pihole -w -q -nr appleid.apple.com
 #WatchESPN
-pihole -w fpdownload.adobe.com 
-pihole -w entitlement.auth.adobe.com 
-pihole -w livepassdl.conviva.com
+pihole -w -q -nr fpdownload.adobe.com 
+pihole -w -q -nr entitlement.auth.adobe.com 
+pihole -w -q -nr livepassdl.conviva.com
 #NVIDIA GeForce Experience
-pihole -w gfwsl.geforce.com
-pihole -w events.gfe.nvidia.com
+pihole -w -q -nr gfwsl.geforce.com
+pihole -w -q -nr events.gfe.nvidia.com
 #Videos not playing in times.com and nydailynews.com
-pihole -w delivery.vidible.tv 
-pihole -w img.vidible.tv 
-pihole -w videos.vidible.tv 
-pihole -w edge.api.brightcove.com 
-pihole -w cdn.vidible.tv
+pihole -w -q -nr delivery.vidible.tv 
+pihole -w -q -nr img.vidible.tv 
+pihole -w -q -nr videos.vidible.tv 
+pihole -w -q -nr edge.api.brightcove.com 
+pihole -w -q -nr cdn.vidible.tv
 #Bing Maps Platform
-pihole -w dev.virtualearth.net 
-pihole -w ecn.dev.virtualearth.net 
-pihole -w t0.ssl.ak.dynamic.tiles.virtualearth.net 
-pihole -w t0.ssl.ak.tiles.virtualearth.net
+pihole -w -q -nr dev.virtualearth.net 
+pihole -w -q -nr ecn.dev.virtualearth.net 
+pihole -w -q -nr t0.ssl.ak.dynamic.tiles.virtualearth.net 
+pihole -w -q -nr t0.ssl.ak.tiles.virtualearth.net
 #Google Play Android updates
-pihole -w android.clients.google.com
+pihole -w -q -nr android.clients.google.com
 #Moto phones OS updates
-pihole -w appspot-preview.l.google.com
+pihole -w -q -nr appspot-preview.l.google.com
 #Captive-portal tests
 #Android/Chrome
-pihole -w connectivitycheck.android.com 
-pihole -w android.clients.google.com 
-pihole -w clients3.google.com 
-pihole -w connectivitycheck.gstatic.com 
+pihole -w -q -nr connectivitycheck.android.com 
+pihole -w -q -nr android.clients.google.com 
+pihole -w -q -nr clients3.google.com 
+pihole -w -q -nr connectivitycheck.gstatic.com 
 #Windows/Microsoft
-pihole -w msftncsi.com 
-pihole -w www.msftncsi.com 
-pihole -w ipv6.msftncsi.com
+pihole -w -q -nr msftncsi.com 
+pihole -w -q -nr www.msftncsi.com 
+pihole -w -q -nr ipv6.msftncsi.com
 #iOS/Apple
-pihole -w captive.apple.com 
-pihole -w gsp1.apple.com 
-pihole -w www.apple.com 
-pihole -w www.appleiphonecell.com
+pihole -w -q -nr captive.apple.com 
+pihole -w -q -nr gsp1.apple.com 
+pihole -w -q -nr www.apple.com 
+pihole -w -q -nr www.appleiphonecell.com
 #Grand Theft Auto V Online PC
-pihole -w prod.telemetry.ros.rockstargames.com
+pihole -w -q -nr prod.telemetry.ros.rockstargames.com
 #Chevrolet Couldn't browse inventory w/o whitelisting
-pihole -w chevrolet.com
+pihole -w -q -nr chevrolet.com
 #Epic Games Store
-pihole -w tracking.epicgames.com
+pihole -w -q -nr tracking.epicgames.com
 #Red Hat Online Learning (subscription required)
-pihole -w 79423.analytics.edgekey.net
+pihole -w -q -nr 79423.analytics.edgekey.net
 #Lowe's Checkout
-pihole -w assets.adobedtm.com
+pihole -w -q -nr assets.adobedtm.com
 #Home Depot Checkout
-pihole -w nexus.ensighten.com
+pihole -w -q -nr nexus.ensighten.com
 #Mozilla Firefox Tracking Protection
-pihole -w tracking-protection.cdn.mozilla.net
+pihole -w -q -nr tracking-protection.cdn.mozilla.net
+
+#Restart DNS
+pihole restartdns
